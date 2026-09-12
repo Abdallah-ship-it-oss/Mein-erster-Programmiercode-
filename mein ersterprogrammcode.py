0861c96a04522dda9e6058e17cc282f7b7674d76
Abdallah-ship-it-oss <fighter7123@gmail.com>
2026-09-12 14:12:46 +0300

    Add files via upload

---
+++ mein ersterprogrammcode.py
@@ -0,0 +1,18 @@
+# das ist mein erster programmcode-Abdallah
+
+stimmung = "neutral"
+note = 5
+
+if note <= 3 :
+	stimmung = "glücklich"
+	print("ich freue mich so .")
+else :
+	if note == 4 :
+		stimmung = "entspannt"
+		print("das ist in ordnung")
+	else :
+		stimmung = "traurig"
+		print("das ist mies gelaufen")
+		
+		#das ist sehr einfach :)
+		
