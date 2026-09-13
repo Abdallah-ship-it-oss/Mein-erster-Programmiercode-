db82a216d3a3bec41ed99674d006b9297859cb4d
Abdallah-ship-it-oss <fighter7123@gmail.com>
2026-09-13 15:34:23 +0300

    Add files via upload

---
+++ Geheimzahl_Spiele.py
@@ -0,0 +1,10 @@
+
+#heute mache ich ein kleiner programmcode
+
+mein_geheimzahl = 3501
+if mein_geheimzahl < 3501 :
+    print("gehimezahl ist falsche")
+elif mein_geheimzahl > 3501 :
+    print("gehimezahl ist falsche")
+else :
+    print("geöffnet")
